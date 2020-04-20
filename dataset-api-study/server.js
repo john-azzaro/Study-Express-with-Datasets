12523ad43165e353eafbf2f1889286bd6a54ed3c
John Azzaro <jazzaro@temple.edu>
2020-04-19 22:21:51 -0700

    updated

--- dataset-api-study/server.js
+++ dataset-api-study/server.js
@@ -5,6 +5,11 @@ const app = express()
 
 app.use(morgan('dev'))
 
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
 
 app,get('/types', handleGetTypes);
 
